28b5289bfeadba7a0168bf5a5edf334b667f4fb5
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2014-04-17 09:22:41 -0400

    Added new initial commit.

---
+++ src/Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public sealed class ConstantRotation :
+    MonoBehaviour
+{
+    [SerializeField]
+    private float _degreesPerSecond = 90.0f;
+    [SerializeField]
+    private Vector3 _rotationAxis = new Vector3(1, 0, 0);
+
+    public void Update()
+    {
+        transform.Rotate(_rotationAxis, _degreesPerSecond * Time.deltaTime);
+    }
+}
