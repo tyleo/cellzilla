37706e919ac5f57261a71394faf0c7ba7e21d637
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2014-04-22 19:37:28 -0400

    Prepared solution for full array implementation.

--- src/Assets/Scripts/Tyleo/MarchingCubes/MarchingCubesEnvironment.cs
+++ src/Assets/Scripts/Tyleo/MarchingCubes/MarchingCubesEnvironment.cs
@@ -7,6 +7,10 @@ namespace Tyleo.MarchingCubes
     public sealed class MarchingCubesEnvironment :
         MonoBehaviour
     {
+        private const int X_INDEX = 0;
+        private const int Y_INDEX = 1;
+        private const int Z_INDEX = 2;
+
         [SerializeField]
         private int _cubesAlongX = 20;
         [SerializeField]
@@ -22,286 +26,152 @@ namespace Tyleo.MarchingCubes
 
         private void Start()
         {
-            var points = CreatePoints(_cubesAlongX, _cubesAlongY, _cubesAlongZ);
-            var edgeDictionary = CreateEdges(points);
-            _cubes = CreateCubes(points, null);
+            _cubes = CreateCubes(_cubesAlongX, _cubesAlongY, _cubesAlongZ);
         }
 
-        private static MarchingPoint[, ,] CreatePoints(int cubesAlongX, int cubesAlongY, int cubesAlongZ)
+        private static MarchingCube[, ,] CreateCubes(int cubesAlongX, int cubesAlongY, int cubesAlongZ)
         {
             MsDebug.Assert(cubesAlongX > 0);
             MsDebug.Assert(cubesAlongY > 0);
             MsDebug.Assert(cubesAlongZ > 0);
 
-            var result = new MarchingPoint[cubesAlongX + 1, cubesAlongY + 1, cubesAlongZ + 1];
+            var points = new MarchingPoint[cubesAlongX + 1, cubesAlongY + 1, cubesAlongZ + 1];
 
-            for (int i = 0; i < result.GetLength(0); ++i )
+            var xEdges = new MarchingEdge[cubesAlongX + 0, cubesAlongY + 1, cubesAlongZ + 1];
+            var yEdges = new MarchingEdge[cubesAlongX + 1, cubesAlongY + 0, cubesAlongZ + 1];
+            var zEdges = new MarchingEdge[cubesAlongX + 1, cubesAlongY + 1, cubesAlongZ + 0];
+
+            var cubes = new MarchingCube[cubesAlongX, cubesAlongY, cubesAlongZ];
+
+            CreateInitialXYZVertex(points);
+
+            CreateInitialXYEdge(points, zEdges);
+            CreateInitialXZEdge(points, yEdges);
+            CreateInitialYZEdge(points, xEdges);
+
+            CreateInitialXFace(points, yEdges, zEdges);
+            CreateInitialYFace(points, xEdges, zEdges);
+            CreateInitialZFace(points, xEdges, yEdges);
+
+            CreateXYZCubes(points, cubes, xEdges, yEdges, zEdges);
+
+            return cubes;
+        }
+
+        private static void CreateInitialXYZVertex(MarchingPoint[, ,] points)
+        {
+            const int i = 0;
+            const int j = 0;
+            const int k = 0;
+
+            points[i, j, k] = null;
+        }
+
+        private static void CreateInitialXYEdge(MarchingPoint[, ,] points, MarchingEdge[, ,] zEdges)
+        {
+            const int i = 0;
+            const int j = 0;
+
+            for (int k = 1; k < points.GetLength(Z_INDEX); ++k)
             {
-                for (int j = 0; j < result.GetLength(1); ++j)
+                points[i, j, k] = null;
+
+                zEdges[i + 0, j + 0, k - 1] = null;
+            }
+        }
+
+        private static void CreateInitialXZEdge(MarchingPoint[, ,] points, MarchingEdge[, ,] yEdges)
+        {
+            const int i = 0;
+            const int k = 0;
+
+            for (int j = 1; j < points.GetLength(Y_INDEX); ++j)
+            {
+                points[i, j, k] = null;
+
+                yEdges[i + 0, j - 1, k + 0] = null;
+            }
+        }
+
+        private static void CreateInitialYZEdge(MarchingPoint[, ,] points, MarchingEdge[, ,] xEdges)
+        {
+            const int j = 0;
+            const int k = 0;
+
+            for (int i = 1; i < points.GetLength(X_INDEX); ++i)
+            {
+                points[i, j, k] = null;
+
+                xEdges[i - 1, j + 0, k + 0] = null;
+            }
+        }
+
+        private static void CreateInitialXFace(MarchingPoint[, ,] points, MarchingEdge[, ,] yEdges, MarchingEdge[, ,] zEdges)
+        {
+            const int i = 0;
+
+            for (int j = 1; j < points.GetLength(Y_INDEX); ++j)
+            {
+                for (int k = 1; k < points.GetLength(Z_INDEX); ++k)
                 {
-                    for (int k = 0; k < result.GetLength(2); ++k)
+                    points[i, j, k] = null;
+
+                    yEdges[i + 0, j - 1, k + 0] = null;
+                    zEdges[i + 0, j + 0, k - 1] = null;
+                }
+            }
+        }
+
+        private static void CreateInitialYFace(MarchingPoint[, ,] points, MarchingEdge[, ,] xEdges, MarchingEdge[, ,] zEdges)
+        {
+            const int j = 0;
+
+            for (int i = 1; i < points.GetLength(X_INDEX); ++i)
+            {
+                for (int k = 1; k < points.GetLength(Z_INDEX); ++k)
+                {
+                    points[i, j, k] = null;
+
+                    xEdges[i - 1, j + 0, k + 0] = null;
+                    zEdges[i + 0, j + 0, k - 1] = null;
+                }
+            }
+        }
+
+        private static void CreateInitialZFace(MarchingPoint[, ,] points, MarchingEdge[, ,] xEdges, MarchingEdge[, ,] yEdges)
+        {
+            const int k = 0;
+
+            for (int i = 1; i < points.GetLength(X_INDEX); ++i)
+            {
+                for (int j = 1; j < points.GetLength(Y_INDEX); ++j)
+                {
+                    points[i, j, k] = null;
+
+                    xEdges[i - 1, j + 0, k + 0] = null;
+                    yEdges[i + 0, j - 1, k + 0] = null;
+                }
+            }
+        }
+
+        private static void CreateXYZCubes(MarchingPoint[, ,] points, MarchingCube[, ,] cubes, MarchingEdge[, ,] xEdges, MarchingEdge[, ,] yEdges, MarchingEdge[, ,] zEdges)
+        {
+            for (int i = 1; i < points.GetLength(X_INDEX); ++i)
+            {
+                for (int j = 1; j < points.GetLength(Y_INDEX); ++j)
+                {
+                    for (int k = 1; k < points.GetLength(Z_INDEX); ++k)
                     {
-                        result[i, j, k] = new MarchingPoint(
-                            (float)i - (float)cubesAlongX / 2.0f,
-                            (float)j - (float)cubesAlongY / 2.0f,
-                            (float)k - (float)cubesAlongZ / 2.0f
-                        );
+                        points[i, j, k] = null;
+
+                        xEdges[i - 1, j + 0, k + 0] = null;
+                        yEdges[i + 0, j - 1, k + 0] = null;
+                        zEdges[i + 0, j + 0, k - 1] = null;
+
+                        cubes[i - 1, j - 1, k - 1] = null;
                     }
                 }
             }
-
-            return result;
-        }
-
-        private static Dictionary<MarchingPointPair, MarchingEdge> CreateEdges(MarchingPoint[,,] marchingPoints)
-        {
-            var result = new Dictionary<MarchingPointPair, MarchingEdge>();
-
-            // We add all of the edges except those located on boundaries. We do this to simplify
-            // the logic by avoiding complicated if statements in our loop.
-            AddMostEdgesToDictionary(marchingPoints, result);
-
-            // At this point everything is in our dictionary except the edges on three faces of the
-            // lattice and the edges on the edges of the lattice.
-            AddFaceBoundryXEdgesToDictionary(marchingPoints, result);
-            AddFaceBoundryYEdgesToDictionary(marchingPoints, result);
-            AddFaceBoundryZEdgesToDictionary(marchingPoints, result);
-
-            // At this point we just need to add the edges of the lattice.
-            AddEdgeBoundryXEdgesToDictionary(marchingPoints, result);
-            AddEdgeBoundryYEdgesToDictionary(marchingPoints, result);
-            AddEdgeBoundryZEdgesToDictionary(marchingPoints, result);
-
-            return result;
-        }
-
-        private static void AddMostEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            for (int i = 0; i < marchingPoints.GetLength(0) - 1; ++i)
-            {
-                for (int j = 0; j < marchingPoints.GetLength(1) - 1; ++j)
-                {
-                    for (int k = 0; k < marchingPoints.GetLength(2) - 1; ++k)
-                    {
-                        var currentStartPoint = marchingPoints[i, j, k];
-                        MarchingPoint currentEndPoint;
-                        
-                        currentEndPoint = marchingPoints[i + 0, j + 0, k + 1];
-                        edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-
-                        currentEndPoint = marchingPoints[i + 0, j + 1, k + 0];
-                        edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-
-                        currentEndPoint = marchingPoints[i + 1, j + 0, k + 0];
-                        edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-                    }
-                }
-            }
-        }
-
-        private static void AddFaceBoundryXEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            var i = marchingPoints.GetLength(0) - 1;
-
-            for (int j = 0; j < marchingPoints.GetLength(1) - 1; ++j)
-            {
-                for (int k = 0; k < marchingPoints.GetLength(2) - 1; ++k)
-                {
-                    var currentStartPoint = marchingPoints[i, j, k];
-                    MarchingPoint currentEndPoint;
-
-                    currentEndPoint = marchingPoints[i + 0, j + 0, k + 1];
-                    edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-
-                    currentEndPoint = marchingPoints[i + 0, j + 1, k + 0];
-                    edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-                }
-            }
-        }
-
-        private static void AddFaceBoundryYEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            var j = marchingPoints.GetLength(1) - 1;
-
-            for (int i = 0; i < marchingPoints.GetLength(0) - 1; ++i)
-            {
-                for (int k = 0; k < marchingPoints.GetLength(2) - 1; ++k)
-                {
-                    var currentStartPoint = marchingPoints[i, j, k];
-                    MarchingPoint currentEndPoint;
-
-                    currentEndPoint = marchingPoints[i + 0, j + 0, k + 1];
-                    edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-
-                    currentEndPoint = marchingPoints[i + 1, j + 0, k + 0];
-                    edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-                }
-            }
-        }
-
-        private static void AddFaceBoundryZEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            var k = marchingPoints.GetLength(2) - 1;
-
-            for (int i = 0; i < marchingPoints.GetLength(0) - 1; ++i)
-            {
-                for (int j = 0; j < marchingPoints.GetLength(1) - 1; ++j)
-                {
-                    var currentStartPoint = marchingPoints[i, j, k];
-                    MarchingPoint currentEndPoint;
-
-                    currentEndPoint = marchingPoints[i + 0, j + 1, k + 0];
-                    edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-
-                    currentEndPoint = marchingPoints[i + 1, j + 0, k + 0];
-                    edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-                }
-            }
-        }
-
-        private static void AddEdgeBoundryXEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            var j = marchingPoints.GetLength(1) - 1;
-            var k = marchingPoints.GetLength(2) - 1;
-
-            for (int i = 0; i < marchingPoints.GetLength(0) - 1; ++i)
-            {
-                var currentStartPoint = marchingPoints[i, j, k];
-                MarchingPoint currentEndPoint;
-
-                currentEndPoint = marchingPoints[i + 1, j + 0, k + 0];
-                edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-            }
-        }
-
-        private static void AddEdgeBoundryYEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            var i = marchingPoints.GetLength(0) - 1;
-            var k = marchingPoints.GetLength(2) - 1;
-
-            for (int j = 0; j < marchingPoints.GetLength(1) - 1; ++j)
-            {
-                var currentStartPoint = marchingPoints[i, j, k];
-                MarchingPoint currentEndPoint;
-
-                currentEndPoint = marchingPoints[i + 0, j + 1, k + 0];
-                edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-            }
-        }
-
-        private static void AddEdgeBoundryZEdgesToDictionary(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> edgeDictionary)
-        {
-            var i = marchingPoints.GetLength(0) - 1;
-            var j = marchingPoints.GetLength(1) - 1;
-
-            for (int k = 0; k < marchingPoints.GetLength(2) - 1; ++k)
-            {
-                var currentStartPoint = marchingPoints[i, j, k];
-                MarchingPoint currentEndPoint;
-
-                currentEndPoint = marchingPoints[i + 0, j + 0, k + 1];
-                edgeDictionary.Add(new MarchingPointPair(currentStartPoint, currentEndPoint), new MarchingEdge(currentStartPoint, currentEndPoint));
-            }
-        }
-
-        private static MarchingCube[, ,] CreateCubes(MarchingPoint[, ,] marchingPoints, Dictionary<MarchingPointPair, MarchingEdge> marchingEdgeDictionary)
-        {
-            var result = new MarchingCube[marchingPoints.GetLength(0) - 1, marchingPoints.GetLength(1) - 1, marchingPoints.GetLength(2) - 1];
-
-            for (int i = 0; i < result.GetLength(0); ++i)
-            {
-                for (int j = 0; j < result.GetLength(1); ++j)
-                {
-                    for (int k = 0; k < result.GetLength(2); ++k)
-                    {
-                        var nXnYnZPoint = marchingPoints[i + 0, j + 0, k + 0];
-                        var nXnYpZPoint = marchingPoints[i + 0, j + 0, k + 1];
-                        var nXpYnZPoint = marchingPoints[i + 0, j + 1, k + 0];
-                        var nXpYpZPoint = marchingPoints[i + 0, j + 1, k + 1];
-                        var pXnYnZPoint = marchingPoints[i + 1, j + 0, k + 0];
-                        var pXnYpZPoint = marchingPoints[i + 1, j + 0, k + 1];
-                        var pXpYnZPoint = marchingPoints[i + 1, j + 1, k + 0];
-                        var pXpYpZPoint = marchingPoints[i + 1, j + 1, k + 1];
-
-                        var nXnYzZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXnYnZPoint,
-                                nXnYpZPoint
-                            )
-                        ];
-                        var nXpYzZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXpYnZPoint,
-                                nXpYpZPoint
-                            )
-                        ];
-                        var pXnYzZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                pXnYnZPoint,
-                                pXnYpZPoint
-                            )
-                        ];
-                        var pXpYzZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                pXpYnZPoint,
-                                pXpYpZPoint
-                            )
-                        ];
-
-                        var nXzYnZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXnYnZPoint,
-                                nXpYnZPoint
-                            )
-                        ];
-                        var pXzYnZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                pXnYnZPoint,
-                                pXpYnZPoint
-                            )
-                        ];
-                        var nXzYpZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXnYpZPoint,
-                                nXpYpZPoint
-                            )
-                        ];
-                        var pXzYpZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                pXnYpZPoint,
-                                pXpYpZPoint
-                            )
-                        ];
-
-                        var zXnYnZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXnYnZPoint,
-                                pXnYnZPoint
-                            )
-                        ];
-                        var zXnYpZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXnYpZPoint,
-                                pXnYpZPoint
-                            )
-                        ];
-                        var zXpYnZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXpYnZPoint,
-                                pXpYnZPoint
-                            )
-                        ];
-                        var zXpYpZEdge = marchingEdgeDictionary[
-                            new MarchingPointPair(
-                                nXpYpZPoint,
-                                pXpYpZPoint
-                            )
-                        ];
-                    }
-                }
-            }
-
-            return result;
         }
     }
 }
